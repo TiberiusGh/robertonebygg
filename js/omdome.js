7b444d4bbaf752b5c9a80c0e24cd25e102a07077
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-08-28 17:36:09 +0200

    Update code to shuffle the review cards

--- js/omdome.js
+++ js/omdome.js
@@ -74,10 +74,20 @@ const createCard = (card) => `
     `
 
 const renderCards = (target) => {
-  const doubled = [...cardsData, ...cardsData]
+  const shuffled = shuffleArray(cardsData)
+  const doubled = [...shuffled, ...shuffled]
   doubled.forEach((card) =>
     target.insertAdjacentHTML('beforeend', createCard(card))
   )
+}
+
+const shuffleArray = (array) => {
+  const shuffled = [...array]
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+  }
+  return shuffled
 }
 
 export function setupOmdome() {
