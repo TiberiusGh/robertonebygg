1838c529fa7a547c9ad23ed29683222185b3df89
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-08-28 17:01:26 +0200

    Add testimonials code/image

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { hello } from './hello.js'
 import { setupNav } from './nav.js'
+import { setupOmdome } from './omdome.js'
 // import { setupForm } from './form.js'
 
 hello()
 setupNav()
+setupOmdome()
 // setupForm()
